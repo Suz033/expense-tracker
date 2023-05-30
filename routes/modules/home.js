94f6c0c1b47efd8b2c3dff3523344adbf9c12c48
Suz033 <susanamy2324@gmail.com>
2023-05-31 00:33:17 +0800

    feat: add selection function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 // modules
 const express = require('express')
 const router = express.Router()
+const dayjs = require('dayjs')
 
 // files
 const Record = require('../../models/record')
@@ -15,24 +16,72 @@ router.get('/', async (req, res) => {
     const totalAmount = records.reduce(function (total, record) {
       return total + record.amount
     }, 0)
-
+    
+    const categories = await Category.find().lean()
+    
     const modifiedRecords = await Promise.all(records.map(async (record) => {
-      // modify date format: yyyy/mm/dd
-      record.date = record.date.toLocaleDateString('zh-TW', { year: 'numeric', month: '2-digit', day: '2-digit' })
-
-      const category = await Category.findOne({ _id: record.categoryId }).lean()
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
       
-      if (category) {
-        record.icon = category.icon
+      const categoryIcon = await Category.findOne({ _id: record.categoryId }).lean()
+      
+      if (categoryIcon) {
+        record.icon = categories.icon
       }
       
-      return { record, category }
+      return { record, categoryIcon }
     }))
     
-    const { record, category } = modifiedRecords
+    const { record, categoryIcon } = modifiedRecords
     
-    res.render('index', { totalAmount, modifiedRecords })
+    res.render('index', { categories, totalAmount, records: modifiedRecords })
     // console.log(modifiedRecords)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+router.get('/search', async (req, res) => {
+  const userId = req.user._id
+  const categoryId = req.query.categoryId
+
+  if (!categoryId) {
+    return res.redirect('/')
+  }
+
+  try {
+    const categories = await Category.find().lean()
+
+    categories.forEach(item => {
+      if (String(item._id) === categoryId) {
+        item.selected = true
+      } else {
+        item.selected = false
+      }
+    })
+
+    const records = await Record.find({ userId, categoryId })
+      .populate('categoryId')
+      .lean()
+      .sort({ date: 'desc' })
+
+    const totalAmount = records.reduce(function (total, record) {
+      return total + record.amount
+    }, 0)
+
+    const modifiedRecords = await Promise.all(records.map(async (record) => {
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
+
+      const categoryIcon = await Category.findOne({ _id: record.categoryId }).lean()
+
+      if (categoryIcon) {
+        record.icon = categories.icon
+      }
+
+      return { record, categoryIcon }
+    }))
+
+    res.render('index', { records: modifiedRecords, categories, totalAmount })
   } catch (err) {
     console.error(err)
     res.status(500).send('Internal Server Error')
