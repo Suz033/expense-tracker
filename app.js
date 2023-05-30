099e8657cce7f66d4449e53964ed4f3658e4a843
Suz033 <susanamy2324@gmail.com>
2023-05-31 02:18:34 +0800

    feat: add flash message

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
-const usePassport = require('./config/passport')  // after session
+const flash = require('connect-flash')
 
 // files
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 app.use(express.static('public'))
 require('./config/mongoose')
@@ -23,7 +24,7 @@ app.use(methodOverride('_method'))
 
 // session
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -31,18 +32,23 @@ app.use(session({
 // passport
 usePassport(app)  // before routes
 
+// flash message
+app.use(flash())
+
 
 //// auth state ////
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
 
 //// routes ////
 // index
-app.use('/', routes)
+app.use(routes)
 
 // listen port
 const PORT = process.env.PORT || 3000
